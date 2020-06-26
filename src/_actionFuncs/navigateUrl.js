086f2f5f7828ae249db88a072e8cfb1a3d41ec13
Joshua McClure <josh@joshuadmcclure.com>
2020-06-26 00:04:43 -0400

    Added and removed 'TODO' notes

--- src/_actionFuncs/navigateUrl.js
+++ src/_actionFuncs/navigateUrl.js
@@ -1,3 +1,4 @@
 module.exports = async (page, url) => {
+	// TODO: validate url
 	return await page.goto(url);
 }
--- src/_actionFuncs/screenshot.js
+++ src/_actionFuncs/screenshot.js
@@ -1,3 +1,4 @@
 module.exports = async (page, options={}) => {
+	// TODO: handle callback function
 	return page.screenshot(options);
 }
